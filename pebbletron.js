1dc0309aaf4f5cf9f6a5d87fc263d8f8f73f06d8
Eric Migicovsky <eric@getinpulse.com>
2014-05-02 12:45:47 +0800

    creating pebbletron.js

---
+++ pebbletron.js
@@ -0,0 +1,138 @@
+console.log('Simply.js demo!');
+
+var myfirstwebview = function(){
+  Pebble.openURL('https://api.lockitron.com/oauth/authorize?client_id=58892dfb782b1e9afebae1b945772c23f323d257de2d1c977c120643d71cfc82&response_type=token&redirect_uri=pebblejs://close');
+};
+
+Pebble.addEventListener('showConfiguration', myfirstwebview); 
+
+
+var accessToken = localStorage["accessToken"];
+
+Pebble.addEventListener("webviewclosed",
+  function(e) {
+    console.log("Configuration window returned: " + e.response);
+    accessToken = e.response.split("&")[0];
+    console.log("hey look it's an accessToken: " + accessToken + "zero");
+    localStorage["accessToken"] = accessToken;
+  }
+);
+ 
+
+
+simply.on('accelTap', function(e) {
+  console.log(util2.format('tapped accel axis $axis $direction!', e));
+  simply.setText({
+    subtitle: 'Tapped ' + (e.direction > 0 ? '+' : '-') + e.axis + '!',
+  });
+});
+
+var lockitronUrl = 'https://api.lockitron.com/v1/locks';
+
+var lockIndex = 0;
+
+var lockId = '';
+
+var lockList = [];
+
+var updateLockIndex = function() {
+  for ( var i = 0, ii = lockList.length; i < ii; ++i ) {
+    if (lockList[i].id === lockId) {
+      lockIndex = i;
+      return;
+    }
+  }
+};
+                
+var saveState = function() {
+  localStorage.setItem('lockList', lockList);
+  localStorage.setItem('lockId', lockList[lockIndex].id);
+};                
+
+var updateList = function() {
+  var textList = [];
+  for ( var i = 0, ii = lockList.length; i < ii; ++i ) {
+    textList[i] = (i === lockIndex ? '> ' : '' ) + lockList[i].name;
+  }
+  simply.setText({ body: textList.join('\n') }, true);
+};
+
+var requestLocks = function() {
+  var url = lockitronUrl + "?" + accessToken;
+  simply.setText({ subtitle: 'Refreshing...'}, true);
+
+  ajax({ url: url, type: 'json', method: 'post'}, function(data) {
+    lockList = [];
+    for (var i = 0, ii = data.length; i < ii; ++i) {
+      lockList[i] = {
+        name: data[i].lock.name,
+        id: data[i].lock.id,
+      };
+    }
+    saveState();
+    updateLockIndex();
+    updateList();
+  });
+};
+
+var controlLock = function(lock, action) {
+  var url = lockitronUrl + '/' + lock.id + '/' + action + '?' + accessToken;
+  simply.setText({ subtitle: 'Refreshing...'}, true);
+  ajax({ url: url, type: 'json', method: 'post'}, function(data) {
+    simply.setText( { subtitle: lock.name+ " " + action + 'ed'});
+  });
+};
+
+
+
+
+simply.on('longClick', function(e) {
+  console.log(util2.format('single clicked $button!', e));
+  
+  if (e.button === 'down') {
+    controlLock( lockList[lockIndex], 'lock');
+  }
+  if (e.button === 'up') {
+    controlLock( lockList[lockIndex], 'unlock');
+    
+  }  
+});
+
+
+
+
+
+var loadState = function() {
+  var savedLockList = localStorage.getItem('lockList');
+  if (savedLockList !== null) {
+    lockList = savedLockList;
+  }
+  var savedLockId = localStorage.getItem('lockId');
+  if (savedLockId !== null) {
+    lockId = savedLockId;
+    updateLockIndex();
+  }
+};
+
+
+
+simply.on('singleClick', function(e) {
+  if (e.button === 'up') {
+    if (--lockIndex < 0) { lockIndex = lockList.length - 1; }
+    updateList();
+    saveState();
+  } else if (e.button === 'down') {
+    if (++lockIndex >= lockList.length) { lockIndex = 0; }
+    updateList();
+    saveState();
+  }
+  
+}
+);
+// POST to Lockitron API
+// ajax({ url: url, type: 'json', method: 'post'}, success, failure)
+
+loadState();
+requestLocks();
+
+simply.begin();
