863c55af29aab2236cd717b78fe7e6436f939ee2
Eric Migicovsky <eric@getinpulse.com>
2014-06-09 01:58:08 +0800

    initial switch to pebble.js

--- pebbletron.js
+++ pebbletron.js
@@ -1,4 +1,11 @@
-console.log('Simply.js demo!');
+// Welcome to PebbleJS!
+//
+// This is where you write your app.
+
+var UI = require('ui');
+var Vector2 = require('vector2');
+var ajax = require('ajax');
+
 
 var myfirstwebview = function(){
   Pebble.openURL('https://api.lockitron.com/oauth/authorize?client_id=58892dfb782b1e9afebae1b945772c23f323d257de2d1c977c120643d71cfc82&response_type=token&redirect_uri=pebblejs://close');
@@ -6,122 +13,194 @@ var myfirstwebview = function(){
 
 Pebble.addEventListener('showConfiguration', myfirstwebview); 
 
-var accessToken = localStorage["accessToken"];
+var accessToken = localStorage.getItem("accessToken");
 
 Pebble.addEventListener("webviewclosed",
   function(e) {
+    if (e.response === 'CANCELLED') { return; }
     console.log("Configuration window returned: " + e.response);
     accessToken = e.response.split("&")[0];
     console.log("hey look it's an accessToken: " + accessToken + "zero");
-    localStorage["accessToken"] = accessToken;
+    localStorage.setItem("accessToken", accessToken);
   }
 );
  
 
+
 var lockitronUrl = 'https://api.lockitron.com/v2/locks';
-var lockIndex = 0;
-var lockId = '';
 var lockList = [];
+var fav1 = {name: '', id: '', list: '1', state: '',};
+var fav2 = {name: '', id: '', list: '2', state: '',};
+
 
 // menu setup functions
 
 var loadState = function() {
-  var savedLockList = localStorage.getItem('lockList');
-  if (savedLockList !== null) {
-    lockList = savedLockList;
+  console.log('Loading State!'); 
+  var savedFav1 = localStorage.getItem("fav1");
+  if (savedFav1 !== null) {
+    fav1 = JSON.parse(savedFav1);
   }
-  var savedLockId = localStorage.getItem('lockId');
-  if (savedLockId !== null) {
-    lockId = savedLockId;
-    updateLockIndex();
-  }
-};
-
-var updateList = function() {
-  var textList = [];
-  for ( var i = 0, ii = lockList.length; i < ii; ++i ) {
-    textList[i] = (i === lockIndex ? '> ' : '' ) + lockList[i].name;
-  }
-  simply.setText({ body: textList.join('\n') }, true);
+  var savedFav2 = localStorage.getItem("fav2");
+  if (savedFav2 !== null) {
+    fav2 = JSON.parse(savedFav2);
+  }                
 };
 
 var saveState = function() {
-  localStorage.setItem('lockList', lockList);
-  localStorage.setItem('lockId', lockList[lockIndex].id);
+  localStorage.setItem("fav1", JSON.stringify(fav1));
+  localStorage.setItem("fav2", JSON.stringify(fav2));
 };  
 
-var updateLockIndex = function() {
-  for ( var i = 0, ii = lockList.length; i < ii; ++i ) {
-    if (lockList[i].id === lockId) {
-      lockIndex = i;
-      return;
-    }
-  }
+var deleteState = function() {
+  lockList = [];
+  fav1 = {name: '', id: '',};
+  fav2 = {name: '', id: '',};
+  localStorage.clear();
 };
 
-
-// Control locks
+// get list of locks
 var requestLocks = function() {
+  console.log('requesting locks!');
   var url = lockitronUrl + "?" + accessToken;
-  simply.setText({ subtitle: 'Refreshing...'}, true);
-
+  console.log("requesting locks url: " + url);
+  
   ajax({ url: url, type: 'json', method: 'get'}, function(data) {
     lockList = [];
+    console.log("data.length " + data.length);
     for (var i = 0, ii = data.length; i < ii; ++i) {
       lockList[i] = {
         name: data[i].name,
         id: data[i].id,
+        state: data[i].state,
       };
+    console.log("lock name: " + lockList[i].name + " lock id: " + lockList[i].id);
     }
-    saveState();
-    updateLockIndex();
-    updateList();
   });
 };
 
+// Control locks
 var controlLock = function(lock, action) {
-  var url = lockitronUrl + '/' + lock.id + '?' + accessToken + "&state=" + action;
-  simply.setText({ subtitle: 'Refreshing...'}, true);
+  var url = lockitronUrl + '/' + lock.id + '?' + accessToken + '&state=' + action;
+  console.log("control lock url: " + lock.name);
   ajax({ url: url, type: 'json', method: 'put'}, function(data) {
-    simply.setText( { subtitle: lock.name+ " " + action + 'ed'});
+    console.log("data.state: " + data.state);
+    if(lock.list == '1'){
+      fav1.state = data.state;
+      menu.item(0,0, {title: fav1.name, subtitle: fav1.state + "ed" });
+    }
+    if(lock.list == '2'){
+      fav2.state = data.state;
+      menu.item(0,1, {title: fav2.name, subtitle: fav2.state + "ed" });
+    }
+    
   });
 };
 
-   
 
-// Simply.js functions
+                
+// starting pebble.js                
 
-simply.on('singleClick', function(e) {
-  if (e.button === 'up') {
-    if (--lockIndex < 0) { lockIndex = lockList.length - 1; }
-    updateList();
-    saveState();
-  } else if (e.button === 'down') {
-    if (++lockIndex >= lockList.length) { lockIndex = 0; }
-    updateList();
-    saveState();
-  }
-  
-}
-);
+console.log("accessToken = " + accessToken);
+console.log("fav1 = " + fav1.name);
+console.log("fav2 = " + fav2.name);
+loadState();
+console.log("accessToken = " + accessToken);
+console.log("fav1 = " + fav1.name);
+console.log("fav2 = " + fav2.name);
 
-simply.on('longClick', function(e) {
-  console.log(util2.format('single clicked $button!', e));
-  
-  if (e.button === 'down') {
-    controlLock( lockList[lockIndex], 'lock');
-  }
-  if (e.button === 'up') {
-    controlLock( lockList[lockIndex], 'unlock');
-    
+var menu = new UI.Menu();
+menu.items(0, [ { title: fav1.name, subtitle: fav2.state + "ed" }, { title: fav2.name, subtitle: fav2.state + "ed" }, { title: 'Getting Locklist' }, { title: 'toggle favourite' }, {title: 'Delete locks'}, {title: 'print locklist'}, {title: 'select fav1'},{title: 'select fav2'} ]);
+menu.show();
+
+
+menu.on('select', function(e) {
+  console.log("Menu item clicked: " + e.item);
+  if (e.item == '0') {
+    console.log( "favourite lock: " + fav1.name + ' id: ' + fav1.id);
+    controlLock( fav1, 'toggle');
   }  
+  if (e.item == '1') {
+    console.log( "favourite lock: " + fav2.name + ' id: ' + fav2.id);
+    controlLock( fav2, 'toggle');
+  }  
+  if (e.item == '2') {
+    requestLocks();
+  }
+  if (e.item == '3') {
+    console.log( "favourite lock: " + fav1.name + ' id: ' + fav1.id);
+    controlLock( fav1, 'toggle');
+  }
+  if (e.item == '4') {
+    deleteState();
+  }
+  if (e.item == '5') {
+    for (var i = 0, ii = lockList.length; i < ii; ++i) {
+      lockList[i] = {
+        name: lockList[i].name,
+        id: lockList[i].id,
+      };
+    console.log("lock name: " + lockList[i].name + " lock id: " + lockList[i].id);
+    }
+  }
+  if (e.item == '6') {
+    console.log('throwing up fMenu');
+    
+   var fMenu = new UI.Menu({ sections: [{ items: [] }] });
+    for (var y = 0, yy = lockList.length; y < yy; ++y) {
+    console.log( "title: " + lockList[y].name);
+      fMenu.item(0, y, {title: lockList[y].name});
+    }
+    fMenu.show();
+    fMenu.on('select', function(e) {
+      fav1.name = lockList[e.item].name;
+      fav1.id = lockList[e.item].id;
+      fav1.state = lockList[e.item].state;
+      console.log( 'fav1 lock: ' + fav1.name);
+      
+      fMenu.hide();
+      saveState();
+    });
+  }
+  if (e.item == '7') {
+    console.log('throwing up f2Menu');
+    
+   var f2Menu = new UI.Menu({ sections: [{ items: [] }] });
+    for (var y = 0, yy = lockList.length; y < yy; ++y) {
+    console.log( "title: " + lockList[y].name);
+      f2Menu.item(0, y, {title: lockList[y].name});
+    }
+    f2Menu.show();
+    f2Menu.on('select', function(e) {
+      fav2.name = lockList[e.item].name;
+      fav2.id = lockList[e.item].id;
+      fav2.state = lockList[e.item].state;
+      console.log( 'fav2 lock: ' + fav2.name);
+      
+      f2Menu.hide();
+      saveState();
+    });
+  }
+         
 });
 
+var Accel = require('ui/accel');
+Accel.init();
+Accel.on('tap', function(e) {
+  console.log('Tap event on axis: ' + e.axis + ' and direction: ' + e.direction);
+  if (e.axis === 'x'){
+    controlLock( fav1, 'unlock');
+  }
+  if (e.axis === 'y'){
+    controlLock( fav1, 'lock');
+  }
+  
+  if (e.axis === 'z' && e.direction > 0 ){
+    controlLock( fav1, 'unlock');
+  }
+  if (e.axis === 'z' && e.direction < 0 ){
+    controlLock( fav1, 'lock');
+  } 
+});
+   
 
-// POST to Lockitron API
-// ajax({ url: url, type: 'json', method: 'post'}, success, failure)
-
-loadState();
-requestLocks();
-
-simply.begin();
